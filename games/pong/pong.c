a1835110153f07eddd83049d1d2420e7d740f4d7
ragnarlodbrok1992 <maraus92@gmail.com>
2024-01-26 21:49:07 +0100

    WIP

--- games/pong/pong.c
+++ games/pong/pong.c
@@ -2,19 +2,85 @@
 
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
+#include <stdbool.h>
+
+// Const stuff
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+bool ENGINE_RUNNING = true;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 int main(void)
 {
-    printf("Pong Game in SDL.\n");
+  printf("Pong Game in SDL.\n");
 
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
+      printf("SDL_Init Error: %s\n", SDL_GetError());
+      return 1;
+  }
+
+  // Create a window
+  window = SDL_CreateWindow("Pong Game",
+                            SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED,
+                            WINDOW_WIDTH,
+                            WINDOW_HEIGHT,
+                            SDL_WINDOW_SHOWN);
+  if (window == NULL)
+  {
+      printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+      SDL_Quit();
+      return 1;
+  }
+
+  // Create a renderer
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == NULL)
+  {
+      SDL_DestroyWindow(window);
+      printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+      SDL_Quit();
+      return 1;
+  }
+
+  // Main game loop
+  while (ENGINE_RUNNING)
+  {
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
     {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
+      // Internal events
+      if (event.type == SDL_QUIT)
+      {
+        ENGINE_RUNNING = false;
+      }
+      // Keypresses
+      if (event.type == SDL_KEYDOWN)
+      {
+        switch (event.key.keysym.sym)
+        {
+          case SDLK_ESCAPE:
+            ENGINE_RUNNING = false;
+            break;
+          default:
+            break;
+        }
+      }
     }
 
-    printf("All went well - goodbye!\n");
-    return 0;
+    // Clear the screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw the screen
+    SDL_RenderPresent(renderer);
+  }
+
+
+  printf("All went well - goodbye!\n");
+  return 0;
 }
 
